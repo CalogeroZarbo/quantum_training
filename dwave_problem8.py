8931799ff8ba417571ecaca921bd9293a789d4d5
Calogero Zarbo <calogero.zarbo@docebo.com>
2019-03-17 10:46:31 +0100

    Problem 8

---
+++ dwave_problem8.py
@@ -0,0 +1,36 @@
+# Calogero Zarbo, Docebo, 17-Mar-2019
+# D-Wave Challenge 8
+
+#   NAE3SAT Truth Table
+# Q-1    Q-2    Q-3    R
+#  0      0      0     1
+#  0      0      1     0
+#  0      1      0     0
+#  0      1      1     0
+#  1      0      0     0
+#  1      0      1     0
+#  1      1      0     0
+#  1      1      1     1
+
+from dwave.system.composites import EmbeddingComposite
+from dwave.system.samplers import DWaveSampler
+import dwavebinarycsp
+import dimod
+import operator
+
+
+def not_all_equal(q1, q2, q3): 
+    return not ((q1 == q2) and (q2 == q3))
+
+csp = dwavebinarycsp.ConstraintSatisfactionProblem(vartype=dimod.Vartype.SPIN) 
+csp.add_constraint(not_all_equal, ['a', 'b', 'c'])
+csp.add_constraint(not_all_equal, ['c', 'd', 'e'])
+
+my_bqm = dwavebinarycsp.stitch(csp)
+
+print(my_bqm)
+
+sampler = EmbeddingComposite(DWaveSampler())
+response = sampler.sample(my_bqm, num_reads = 5000)
+for sample, energy, occurences in response.data(['sample', 'energy', 'num_occurrences']):
+    print(list(sample.values()), 'Occurrences:', occurences, 'Energy:', energy)
